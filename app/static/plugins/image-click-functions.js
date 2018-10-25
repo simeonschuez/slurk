0f78fea99b568fddf9d1632835c86c36c42dfdcb
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2018-10-25 11:08:41 +0200

    added audio feedback

--- app/static/plugins/image-click-functions.js
+++ app/static/plugins/image-click-functions.js
@@ -6,8 +6,10 @@ let mouse = {
     pos: {x:false, y:false},
     pos_prev: false,
 };
-
 let mousePositions = [];
+var audioDescription = document.getElementById('audio-description');
+var audioCorrect = document.getElementById('audio-correct');
+var audioFalse = document.getElementById('audio-false');
 
 function getPosition (evt, area) {
     position = $(area).offset();
@@ -17,7 +19,6 @@ function getPosition (evt, area) {
 
 function emitPosition(a, intrvl) {
     if (mouse.move && mouse.pos_prev) {
-        socket.emit('mouseMove', {coordinates: [ mouse.pos, mouse.pos_prev ],element:a, room: self_room, user: users});
         console.log('mouseMove: ' + mouse.pos.x + ',' + mouse.pos.y);
         mousePositions.push({timestamp:Date.now(), x:mouse.pos.x, y:mouse.pos.y});
         mouse.move = false;
@@ -38,12 +39,18 @@ socket.on('message', function(data) {
     if (data.user.name == "Image_Click_Bot") {
         console.log("message from image click bot")
         if (data.msg.includes("Correct!")) {
+            // play audio, log tracking data, show overlay
+            audioCorrect.play();
             $("#image-overlay").show();
             console.log("logging tracking data");
             socket.emit('log', {type: "mouse_positions", data:mousePositions, room: self_room});
             mousePositions = [];
+        } else if (data.msg.includes("Try again!")) {
+            audioFalse.play();
         } else if (data.msg.includes("Game started!")) {
             gameStarted = true;
+            audioCorrect.src="/static/audio/correct.wav";
+            audioFalse.src="/static/audio/tryagain.wav";
             console.log("game started:", gameStarted);
         } else if (data.msg.includes("No images left")) {
             gameStarted = false;
@@ -65,12 +72,18 @@ socket.on('new_image', function(data) {
 
 socket.on('file_path', function(data) {
     if (data.type == 'audio') {
-        $("#audio-player").attr("src", data.file);
+        audioDescription.src = data.file;
     }
 });
 
 // activate mouse tracking
-trackMovement("#current-image", 50);
+trackMovement(trackingArea, 50);
+
+/* add preload and type attributes to audio elements */
+$(".audio").attr({
+    preload:"auto",
+    type:"audio/wav"
+});
 
 /* send coordinates on click */
 $(trackingArea).click(function(e){
@@ -85,14 +98,14 @@ $('#overlay-button').click(function(e){
     getPosition(e, "#overlay-button");
     socket.emit('mouseClick', {coordinates: mouse.pos,element:"#overlay-button", room: self_room});
     socket.emit('log', {type: "mouse_click", coordinates:mouse.pos,element:"#overlay-button", room: self_room});
-
     $(".overlay").hide();
     $("#replayButton").show();
     /* play transmitted audio file after 500 ms */
-    setTimeout(function(){document.getElementById("audio-player").play(); }, 500);
+    setTimeout(function(){
+        audioDescription.play();}, 500);
 });
 
 /* play audio if replay button is clicked */
 $("#replayButton").click(function(){
-    document.getElementById("audio-player").play();
+    audioDescription.play();
 })
