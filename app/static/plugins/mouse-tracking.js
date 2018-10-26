4d8d99fecfb98ea20e61782c696f16310da11cec
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2018-10-26 14:30:25 +0200

    fixed issues with mouse tracking

--- app/static/plugins/mouse-tracking.js
+++ app/static/plugins/mouse-tracking.js
@@ -13,32 +13,27 @@ function getPosition (evt, area) {
     mouse.pos.y = evt.clientY - position.top;
 }
 
-function emitPosition(a, intrvl) {
-    if (mouse.move && mouse.pos_prev) {
+function emitPosition(a,intrvl) {
+    if (mouse.move) {
+        console.log(mouse.pos, a);
         socket.emit('mousePosition', {
             type:'move',
             coordinates:mouse.pos,
-            element:trackingArea,
+            element:a,
             room:self_room
         });
-        socket.emit('log', {
-            type: "mouse_move",
-            coordinates:{pos:mouse.pos,pos_prev:mouse.pos_prev},
-            element:a,room:self_room
-        });
         mouse.move = false;
-        // console.log('mouseMove: ' + mouse.pos.x + ',' + mouse.pos.y)
     }
-    mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-    setTimeout(emitPosition, intrvl);
+    setTimeout(emitPosition, intrvl, a, intrvl);
 }
 
 function trackMovement(area,interval) {
     $(area).mousemove(function(e){
+        // console.log(new Date());
         getPosition(e, area);
         mouse.move = true;
     });
-    emitPosition(area, interval);
+    emitPosition(area,interval)
 }
 
 // get position within trackingArea every 100 ms
