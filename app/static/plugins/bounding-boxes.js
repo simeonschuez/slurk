4135bb0cba59f69f25f88666d70582ee3538fe54
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2018-11-05 20:35:18 +0100

    made bounding boxes clickable, allow user to delete boxes

--- app/static/plugins/bounding-boxes.js
+++ app/static/plugins/bounding-boxes.js
@@ -1,22 +1,37 @@
 var mouse = {
     click: false,
     move: false,
-    pos: {x:0, y:0},
+    pos: {x:null, y:null},
     drag_start: null
 };
 var rectangle = {
-    p1: {x:null, y:null},
-    p2: {x:null, y:null},
+    p1: null,
+    p2: null,
     width: null,
     height: null
 };
+
 var canvas  = document.getElementById('canvas');
+if (canvas == null) {
+  // create canvas and fit it over #current-image
+  canvas = document.createElement('canvas');
+  var img = document.getElementById('current-image');
+  canvas.id = "canvas";
+  canvas.width = img.clientWidth;
+  canvas.height = img.clientHeight;
+  canvas.style.zIndex = 2;
+  canvas.style.position = "absolute";
+  canvas.style.top = "0px";
+  canvas.style.left = "0px";
+  $("#current-image").wrap( "<div id='image-wrapper' style='position:relative;'></div>");
+  var imgWrapper = document.getElementById("image-wrapper");
+  imgWrapper.appendChild(canvas);
+}
 var context = canvas.getContext('2d');
 context.fillStyle = "rgba(255,0,0,0.2)";
 context.strokeStyle = 'red';
 context.lineWidth = 2;
 
-// Functions for tracking + rectangle
 function getPosition (evt) {
     var canvas_position = $('#canvas').offset();
     mouse.pos.x = evt.clientX - canvas_position.left;
@@ -34,24 +49,60 @@ function drawRectangle(){
     context.stroke();
 }
 
+function normalizeRectangle(){
+  if (rectangle.p1.x > rectangle.p2.x) {
+    var swap = rectangle.p1.x;
+    rectangle.p1.x = rectangle.p2.x;
+    rectangle.p2.x = swap;
+    rectangle.width = rectangle.p2.x - rectangle.p1.x;
+  }
+  if (rectangle.p1.y > rectangle.p2.y) {
+    var swap = rectangle.p1.y;
+    rectangle.p1.y = rectangle.p2.y;
+    rectangle.p2.y = swap;
+    rectangle.height = rectangle.p2.y - rectangle.p1.y;
+  }
+}
+
+function posOnRectangle(){
+  normalizeRectangle();
+  if ((mouse.pos.x >= rectangle.p1.x && mouse.pos.x <= rectangle.p2.x && mouse.pos.y >= rectangle.p1.y && mouse.pos.y <= rectangle.p2.y)) {
+    return true;
+  } else {return false;}
+}
+
 canvas.onmousedown = function(e){
-    getPosition(e)
-    rectangle.p1 = {x: mouse.pos.x, y: mouse.pos.y};
+    getPosition(e);
+    mouse.drag_start = {x: mouse.pos.x, y: mouse.pos.y};
     mouse.click = true;
-    //$("#confirm_rect").attr("disabled", true);
 };
 
 canvas.onmouseup = function(e){
     mouse.click = false;
-    //$("#confirm_rect").attr("disabled", false);
-    console.log(rectangle);
 };
 
 canvas.onmousemove = function(e) {
     getPosition(e);
-    mouse.move = true;
     if(mouse.click) {
+        rectangle.p1 = mouse.drag_start;
+        mouse.move = true;
         // activate drawing rectangles
         drawRectangle();
-    };
+    }
+};
+
+canvas.onclick = function(e) {
+  if (mouse.move == false) {
+    getPosition(e)
+    if (posOnRectangle()) {
+      if (confirm('Is the rectangle set correctly?')) {
+        console.log(rectangle)
+      }
+    } else {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      rectangle = {p1: null, p2: null, width: null, height: null};
+      }
+    } else {
+    mouse.move = false;
+  }
 };
